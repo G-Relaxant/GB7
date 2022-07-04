30acddb384dfcaddadd4bfa0f7f2fd8d1bebe47a
geekbrains <info@geekbrains.ru>
2022-07-04 11:35:03 +0400

    edit

---
+++ 48/Program.cs
@@ -0,0 +1,32 @@
+﻿int m = 5;
+int n = 5;
+
+int[,] array = new int[m, n];
+
+FillArray(array);
+
+PrintArray(array);
+
+
+void PrintArray(int[,] a)
+{
+    for (int i = 0; i < a.GetLength(0); i++)          
+    {
+        for(int j = 0; j < a.GetLength(1); j++)
+        {
+            Console.Write($" {a[i, j]} ");
+        }
+    Console.WriteLine("");
+    }
+}
+
+void FillArray(int [,] a)
+{
+    for(int i = 0; i < a.GetLength(0); i++)
+    {
+        for(int j = 0; j < a.GetLength(1); j++)
+        {
+            a[i, j] = i + j;
+        }
+    }
+}
